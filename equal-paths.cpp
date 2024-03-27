8517155967a51869912a18b81d0bd5ba81e24d4e
Tony Chen <tchen776@usc.edu>
2024-03-27 14:09:24 -0700

    equal-paths completed

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -9,10 +9,48 @@ using namespace std;
 
 // You may add any prototypes of helper functions here
 
+void calculateMaxHeight(Node* root, int currentHeight, int& maxHeight)
+{
+    currentHeight++;
+    
+    if (root->left == nullptr && root->right == nullptr)
+    {
+        if (currentHeight > maxHeight) maxHeight = currentHeight;
+        return;
+    }
+    
+    
+    if (root->left != nullptr) calculateMaxHeight(root->left, currentHeight, maxHeight);
+    if (root->right != nullptr) calculateMaxHeight(root->right, currentHeight, maxHeight);
+}
+
+void calculateMinHeight(Node* root, int currentHeight, int& minHeight)
+{
+    currentHeight++;
+    
+    if (root->left == nullptr && root->right == nullptr)
+    {
+        if (currentHeight < minHeight) minHeight = currentHeight;
+        return;
+    }
+    
+    
+    if (root->left != nullptr) calculateMinHeight(root->left, currentHeight, minHeight);
+    if (root->right != nullptr) calculateMinHeight(root->right, currentHeight, minHeight);
+}
 
 bool equalPaths(Node * root)
 {
     // Add your code below
+    if (root == nullptr) return true;
+    
+    int maxHeight = 0, minHeight = 99999;
+    calculateMaxHeight(root, 0, maxHeight);
+    calculateMinHeight(root, 0, minHeight);
+    
+    if (maxHeight == minHeight) return true;
+    else return false;
+    
 
 }
 
